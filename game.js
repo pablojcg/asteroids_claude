399f461284ec96018e4e6fc40ce739d835a4e9a7
Pablo Castro <pablojcg@gmail.com>
2026-06-07 09:15:30 -0500

    refactor: optimize game loop performance and update collision detection logic in game.js

--- game.js
+++ game.js
@@ -9,14 +9,6 @@ const H = 600;
 const keys = {};
 const justPressed = {};
 
-window.addEventListener('keydown', e => {
-  if (!keys[e.code]) justPressed[e.code] = true;
-  keys[e.code] = true;
-});
-window.addEventListener('keyup', e => {
-  keys[e.code] = false;
-});
-
 
 function pressed(code) {
   const val = justPressed[code];
